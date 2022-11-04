046b704e2f1283b024ee02a1e1387f5f578f36cb
IYADUKUNZE Emile <80049630+emilenfc@users.noreply.github.com>
2022-11-05 01:07:35 +0200

    this is linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,12 @@
 #include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array
+ * @array: arr of ints
+ * @size: size of arr
+ * @value: value to find in arr
+ * Return: print or -1 if fail
+*/
 int linear_search(int *array, size_t size, int value)
 {
 	size_t idx = 0;
